db2404cd90d4ad646db54ab55ed24027413b222b
Alfredo Arias <alfre.arias@gmail.com>
2013-10-26 12:08:43 -0400

    Initial Commit

---
+++ QuakeRadar/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QuakeRadar
+//
+//  Created by Tekhne on 10/26/13.
+//  Copyright (c) 2013 Tekhne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
